23fea8bdbe1e6e7a3430ee8203c9a6215e5eb1b5
Hemant <hmewada74@gmail.com>
2024-03-21 17:47:55 +0530

    removed services route

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/services" element={<Services />} />
+        {/* <Route path="/services" element={<Services />} /> */}
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
